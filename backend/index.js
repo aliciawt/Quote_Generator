fc2c67116159f7f7c1329ffebc8f527d161b6fa5
aliciawt <aliciawynonatjahjadi@gmail.com>
2026-05-11 17:19:45 +0200

    Add backend

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Quotes
+const quotes = [
+  {
+    quote: "Life isn't about getting and having, it's about giving and being.",
+    author: "Kevin Kruse",
+  },
+  {
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    author: "Napoleon Hill",
+  },
+  {
+    quote: "Strive not to be a success, but rather to be of value.",
+    author: "Albert Einstein",
+  },
+];
+
+// Picking random quote
+function pickRandomQuote() {
+  const index = Math.floor(Math.random() * quotes.length);
+  return quotes[index];
+}
+
+// GET, to return a random quote as JSON
+app.get("/", (req, res) => {
+  const quote = pickRandomQuote();
+  res.json(quote);
+});
+
+// POST, to add a new quote
+app.post("/", (req, res) => {
+  const { quote, author } = req.body;
+  
+  if (!quote || !author) {
+    return res.status(400).send("Missing quote or author");
+  }
+  
+  quotes.push({ quote, author });
+  res.send("ok");
+});
+
+// Starting server
+app.listen(port, () => {
+  console.log(`Backend running at http://localhost:${port}`);
+});
